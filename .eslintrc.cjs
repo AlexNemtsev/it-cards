e2bd380ca241a4ba51a415080fa86f2aac258883
Alexander Nemtsev <mrcrazy696@gmail.com>
2024-02-29 14:58:41 +0300

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  extends: '@it-incubator/eslint-config',
+  overrides: [
+    {
+      files: ['**/*.stories.tsx'],
+      rules: {
+        'no-console': 'off',
+        'react-hooks/rules-of-hooks': 'off'
+      },
+    },
+  ],
+  rules: {
+    'react/jsx-curly-brace-presence': 'off',
+  }
+}

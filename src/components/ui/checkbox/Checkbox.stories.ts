0b6871f74383cc6a14da620f492bd0a2500cdaee
mabar84 <mabar@tut.by>
2024-03-07 17:50:59 +0300

    there is problem with pass component as icon-prop in Storybook

--- src/components/ui/checkbox/Checkbox.stories.ts
+++ src/components/ui/checkbox/Checkbox.stories.ts
@@ -1,11 +1,15 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
-import { CheckboxRadix } from './';
+import { CheckboxRadix } from './Checkbox';
 
 const meta = {
   component: CheckboxRadix,
   tags: ['autodocs'],
 } satisfies Meta<typeof CheckboxRadix>;
+
+const a = () => {
+  return '123';
+};
 
 export default meta;
 type Story = StoryObj<typeof meta>;
@@ -13,6 +17,7 @@ type Story = StoryObj<typeof meta>;
 export const ExampleChecked: Story = {
   args: {
     checked: true,
+    children: a(),
   },
 };
 export const ExampleUnChecked: Story = {
--- src/components/ui/checkbox/Checkbox.tsx
+++ src/components/ui/checkbox/Checkbox.tsx
@@ -1,26 +1,26 @@
-import { CheckedMark } from '@/components/ui/checkbox/CheckedMark';
+import { ReactNode } from 'react';
+
 import * as Checkbox from '@radix-ui/react-checkbox';
 
 import s from './Checkbox.module.scss';
 
 type Props = {
+  icon?: ReactNode;
   id?: string;
 } & Parameters<typeof Checkbox.Root>[0];
 export const CheckboxRadix = (props: Props) => {
-  const { checked, disabled, id } = props;
+  const { checked, children, className, disabled, icon, id } = props;
+
+  const rootClassName = `${s.CheckboxRoot} ${className}`;
 
   return (
-    <div className={s.CheckboxWrapper}>
-      <Checkbox.Root
-        className={`${s.CheckboxRoot}`}
-        defaultChecked={checked}
-        disabled={disabled}
-        id={id}
-      >
-        <Checkbox.Indicator>
-          <CheckedMark />
-        </Checkbox.Indicator>
+    <>
+      <Checkbox.Root className={rootClassName} defaultChecked={checked} disabled={disabled} id={id}>
+        <Checkbox.Indicator>{icon}</Checkbox.Indicator>
       </Checkbox.Root>
-    </div>
+      {children}
+    </>
   );
 };
+
+export const bla = () => <p>888</p>;

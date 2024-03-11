bb33c72e9aaf85509208b828ecb478c4214eaaaa
mabar84 <mabar@tut.by>
2024-03-11 21:35:33 +0300

    clsx added, styles fixed, directory checkbox renamed to Checkbox

--- src/components/ui/Checkbox/Checkbox.tsx
+++ src/components/ui/Checkbox/Checkbox.tsx
@@ -1,6 +1,7 @@
 import { ComponentPropsWithoutRef, ReactNode } from 'react';
 
 import * as Checkbox from '@radix-ui/react-checkbox';
+import clsx from 'clsx';
 
 import s from './Checkbox.module.scss';
 
@@ -8,18 +9,13 @@ type Props = {
   icon?: ReactNode;
   id?: string;
 } & ComponentPropsWithoutRef<typeof Checkbox.Root>;
-
-// type Props = ComponentPropsWithoutRef<typeof Checkbox.Root>;
 export const CheckboxRadix = (props: Props) => {
   const { checked, className, disabled, icon, id } = props;
-
-  const rootClassName = `${s.CheckboxRoot} ${className}`;
+  const classNames = clsx(s.CheckboxRoot, className);
 
   return (
-    <>
-      <Checkbox.Root className={rootClassName} defaultChecked={checked} disabled={disabled} id={id}>
-        <Checkbox.Indicator>{icon}</Checkbox.Indicator>
-      </Checkbox.Root>
-    </>
+    <Checkbox.Root className={classNames} defaultChecked={checked} disabled={disabled} id={id}>
+      <Checkbox.Indicator>{icon}</Checkbox.Indicator>
+    </Checkbox.Root>
   );
 };

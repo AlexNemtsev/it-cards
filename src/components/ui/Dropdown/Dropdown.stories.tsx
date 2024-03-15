3d498cce50c16d2f4d5ea29d49ad4d899b721afc
mabar84 <mabar@tut.by>
2024-03-15 21:29:24 +0300

    Story DropdownWithAvatar added

--- src/components/ui/Dropdown/Dropdown.stories.tsx
+++ src/components/ui/Dropdown/Dropdown.stories.tsx
@@ -2,10 +2,17 @@ import type { Meta, StoryObj } from '@storybook/react';
 
 import { Burger } from '@/assets/icons/Burger/Burger';
 import { Delete } from '@/assets/icons/Delete/Delete';
+import { DropdownMenuAvatar } from '@/assets/icons/DropdownMenuAvatar';
 import { Edit } from '@/assets/icons/Edit/Edit';
 import { Learn } from '@/assets/icons/Learn/Learn';
+import { Profile } from '@/assets/icons/Profile/Profile';
+import { SignOut } from '@/assets/icons/SignOut/SignOut';
 import { DropdownItem } from '@/components/ui/Dropdown/DropdownItem';
+import { DropdownItemDivider } from '@/components/ui/Dropdown/DropdownItemDivider';
+import { DropdownProfileInfo } from '@/components/ui/Dropdown/DropdownProfileInfo';
 import { Typography } from '@/components/ui/Typography';
+
+import DropdownProfileInfoStyles from './DropdownProfileInfo/DropdownProfileInfo.module.scss';
 
 import { Dropdown } from './Dropdown';
 const meta = {
@@ -26,10 +33,12 @@ export const DropdownWithBurger: Story = {
           <Learn />
           <Typography.Caption>Learn</Typography.Caption>
         </DropdownItem>
+        <DropdownItemDivider />
         <DropdownItem>
           <Edit />
           <Typography.Caption>Edit</Typography.Caption>
         </DropdownItem>
+        <DropdownItemDivider />
         <DropdownItem>
           <Delete />
           <Typography.Caption>Delete</Typography.Caption>
@@ -37,5 +46,33 @@ export const DropdownWithBurger: Story = {
       </>
     ),
     icon: <Burger />,
+  },
+};
+export const DropdownWithAvatar: Story = {
+  args: {
+    children: (
+      <>
+        <DropdownProfileInfo>
+          <DropdownMenuAvatar />
+          <div>
+            <Typography.Subtitle2>Ivan</Typography.Subtitle2>
+            <Typography.Caption className={DropdownProfileInfoStyles.email}>
+              j&johnson@gmail.com
+            </Typography.Caption>
+          </div>
+        </DropdownProfileInfo>
+        <DropdownItemDivider />
+        <DropdownItem>
+          <Profile />
+          <Typography.Caption>My Profile</Typography.Caption>
+        </DropdownItem>
+        <DropdownItemDivider />
+        <DropdownItem>
+          <SignOut />
+          <Typography.Caption>Sign Out</Typography.Caption>
+        </DropdownItem>
+      </>
+    ),
+    icon: <DropdownMenuAvatar />,
   },
 };

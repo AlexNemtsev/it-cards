ae06cafb56ee323b03aa977d4259c6139cff4dc4
EvgenyStrunevsky <esstrynevskiy@gmail.com>
2024-03-29 00:47:24 +0300

    fix/8_input_component-clear-app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,3 @@
-import { Input } from './components/ui/Input';
-
 export const App = () => {
-  return (
-    <div style={{ margin: 100 }}>
-      <Input label="Input" placeholder="Input" type="search" />
-    </div>
-  );
+  return <div>hello</div>;
 };

924651ab53b1483ac95d58fe682316c6d6c9c582
mabar84 <mabar@tut.by>
2024-04-01 20:52:40 +0300

    component Select added

--- src/components/ui/Pagination/Pagination.tsx
+++ src/components/ui/Pagination/Pagination.tsx
@@ -1,3 +1,6 @@
+import { Select } from '@/components/ui/Select';
+import { Typography } from '@/components/ui/Typography';
+
 import s from './Pagination.module.scss';
 
 import { PagintionButtons } from './PagintionButtons';
@@ -29,6 +32,22 @@ export const Pagination = (props: Props) => {
         totalPages={totalPages}
       />
       <ArrowButton disabled={currentPage >= totalPages} onClick={toNextPageHandler} />
+
+      <Typography.Body2>Показать</Typography.Body2>
+
+      <Select
+        isSmall
+        label="label"
+        placeholder="10"
+        values={[
+          { id: 1, value: '10' },
+          { id: 2, value: '20' },
+          { id: 3, value: '30' },
+          { id: 4, value: '40' },
+          { id: 5, value: '50' },
+        ]}
+      />
+      <Typography.Body2>на странице</Typography.Body2>
     </div>
   );
 };

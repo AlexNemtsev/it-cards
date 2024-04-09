fe13700d5814bee54cd7a7265ae2d542904e4928
EvgenyStrunevsky <esstrynevskiy@gmail.com>
2024-04-09 15:37:05 +0300

    feature/11_add_table_component-delete-className-from-tr-tbody

--- src/components/ui/Table/TableBody/TableBody.tsx
+++ src/components/ui/Table/TableBody/TableBody.tsx
@@ -1,12 +1,6 @@
 import { ComponentPropsWithoutRef, ElementRef, forwardRef } from 'react';
 
-import clsx from 'clsx';
-
 export type Props = ComponentPropsWithoutRef<'tbody'>;
 export const TableBody = forwardRef<ElementRef<'tbody'>, Props>((props: Props, ref) => {
-  const { className, ...restProps } = props;
-
-  const classNames = clsx(className);
-
-  return <tbody className={classNames} ref={ref} {...restProps} />;
+  return <tbody ref={ref} {...props} />;
 });

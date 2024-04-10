9dd8f1427337b47e0552a04d54f13bc0840b6807
mabar84 <mabar@tut.by>
2024-04-10 08:28:19 +0300

    started work about LoginForm

---
+++ src/components/FormCheckbox/FormCheckbox.tsx
@@ -0,0 +1,28 @@
+import { FieldValues, UseControllerProps, useController } from 'react-hook-form';
+
+import { Checkbox } from '@/components/ui/Checkbox';
+
+type Props<T extends FieldValues> = UseControllerProps<T> &
+  Omit<CheckboxProps, 'defaultValue' | 'icon' | 'onChange' | 'value'>;
+export const FormCheckbox = <T extends FieldValues>(props: Props<T>) => {
+  const { control, defaultValue, disabled, errorMessage, name } = props;
+
+  const {
+    field: { onChange, ref, value },
+    fieldState: { error },
+  } = useController({
+    control,
+    defaultValue,
+    name,
+  });
+
+  return (
+    <Checkbox
+      checked={value}
+      disabled={disabled}
+      name={name}
+      onCheckedChange={onChange}
+      ref={ref}
+    />
+  );
+};
---
+++ src/components/auth/LoginForm/LoginForm.tsx
@@ -0,0 +1,45 @@
+import { useForm } from 'react-hook-form';
+
+import { FormCheckbox } from '@/components/FormCheckbox/FormCheckbox';
+import { Button } from '@/components/ui/Button';
+import { Input } from '@/components/ui/Input';
+import { DevTool } from '@hookform/devtools';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from 'zod';
+
+const loginScheme = z.object({
+  email: z.string().email(),
+  password: z.string().min(3),
+  rememberMe: z.boolean().optional(),
+});
+
+type FormValues = z.infer<typeof loginScheme>;
+
+export const LoginForm = () => {
+  const {
+    control,
+    formState: { errors },
+    handleSubmit,
+    register,
+  } = useForm<FormValues>({
+    delayError: 2000,
+    resolver: zodResolver(loginScheme),
+  });
+
+  console.log(errors);
+  const onSubmit = (data: FormValues) => {
+    console.log(data);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Input {...register('email')} error={errors.email?.message} label="email" />
+      <Input {...register('password')} error={errors.password?.message} label="password" />
+
+      <FormCheckbox control={control} errorMessage="123" name="rememberMe" />
+
+      <Button type="submit">Submit</Button>
+      <DevTool control={control} />
+    </form>
+  );
+};

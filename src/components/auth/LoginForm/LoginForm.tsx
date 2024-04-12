9e1a01c765b04a2fc767b185bc32bf77b0c70aaa
mabar84 <mabar@tut.by>
2024-04-12 14:15:39 +0300

    styles for LoginForm added

--- src/components/auth/LoginForm/LoginForm.tsx
+++ src/components/auth/LoginForm/LoginForm.tsx
@@ -3,8 +3,11 @@ import { useForm } from 'react-hook-form';
 import { FormCheckbox } from '@/components/FormCheckbox/FormCheckbox';
 import { Button } from '@/components/ui/Button';
 import { Input } from '@/components/ui/Input';
+import { Typography } from '@/components/ui/Typography';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
+
+import s from './LoginForm.module.scss';
 
 const loginScheme = z.object({
   email: z.string().email(),
@@ -31,13 +34,38 @@ export const LoginForm = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <Input {...register('email')} error={errors.email?.message} label="email" />
-      <Input {...register('password')} error={errors.password?.message} label="password" />
+    <form className={s.form} onSubmit={handleSubmit(onSubmit)}>
+      <Typography.H1>SIgn In</Typography.H1>
 
-      <FormCheckbox control={control} name="rememberMe" />
+      <Input {...register('email')} error={errors.email?.message} label="Email" />
+      <Input
+        {...register('password')}
+        error={errors.password?.message}
+        label="Password"
+        type="password"
+      />
 
-      <Button type="submit">Submit</Button>
+      <FormCheckbox
+        className={s.checkbox}
+        control={control}
+        label="Remember me"
+        name="rememberMe"
+      />
+
+      <Typography.Body2 as="a" className={s.forgot}>
+        Forgot Password?
+      </Typography.Body2>
+      <Button fullWidth type="submit">
+        Sign In
+      </Button>
+
+      <Typography.Body2 as="a" className={s.dontHaveAccount}>
+        Don&apos;t have an account?
+      </Typography.Body2>
+
+      <Typography.Subtitle1 as="a" className={s.signUp}>
+        Sign Up
+      </Typography.Subtitle1>
     </form>
   );
 };

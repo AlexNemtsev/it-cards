61d99177571c33b5bd8aaa3ace96c8441adffe16
EvgenyStrunevsky <esstrynevskiy@gmail.com>
2024-04-17 15:51:45 +0300

    feature/51-signUp-clear-App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,3 @@
-import { LogUpForm } from './components/auth/LogUpForm';
-
 export const App = () => {
-  return (
-    <div>
-      <LogUpForm onSubmit={data => console.log(data)} />
-    </div>
-  );
+  return <div>] hello</div>;
 };

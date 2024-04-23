9aff3a7d078f306538940528aae6c191a1f39da6
mabar84 <mabar@tut.by>
2024-04-23 12:44:59 +0300

    forgot added

---
+++ src/components/auth/ForgotPassword/ForgotPassword.stories.tsx
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { ForgotPassword, FormValues } from '@/components/auth/ForgotPassword';
+
+const meta = {
+  component: ForgotPassword,
+  tags: ['autodocs'],
+  title: 'Auth/ForgotPassword',
+} satisfies Meta<typeof ForgotPassword>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+export const Example: Story = {
+  args: {
+    onSubmit: (data: FormValues) => {
+      console.log(data.email);
+    },
+  },
+  render: ({ onSubmit }) => {
+    return <ForgotPassword onSubmit={onSubmit} />;
+  },
+};

32152105c25a36069349c16172ede8370ee82038
Alexander Nemtsev <mrcrazy696@gmail.com>
2024-05-02 17:42:00 +0300

    Добавлена заготовка для layout

--- src/app/router/router.tsx
+++ src/app/router/router.tsx
@@ -3,20 +3,21 @@ import { RouteObject, createBrowserRouter } from 'react-router-dom';
 import { LoginPage } from '@/pages/LoginPage';
 import { MainPage } from '@/pages/MainPage';
 import { Routes } from '@/shared/constants/routes';
+import { Layout } from '@/shared/ui/Layout';
 
 import { PrivateRoutes } from './PrivateRoutes';
 
 const publicRoutes: RouteObject[] = [
   {
-    element: <LoginPage />,
-    path: Routes.LOGIN,
+    children: [{ element: <LoginPage />, path: Routes.LOGIN }],
+    element: <Layout />,
   },
 ];
 
 const privateRoutes: RouteObject[] = [
   {
-    element: <MainPage />,
-    path: Routes.MAIN,
+    children: [{ element: <MainPage />, path: Routes.MAIN }],
+    element: <Layout />,
   },
 ];
 

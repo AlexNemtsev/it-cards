889713bf20ed0603e90cf29e3cc036d2f00ad4df
Alexander Nemtsev <mrcrazy696@gmail.com>
2024-05-03 10:26:02 +0300

    Добавлена стилизация Layout

---
+++ src/shared/assets/icons/IncubatorLogo/index.ts
@@ -0,0 +1,1 @@
+export { IncubatorLogo } from './IncubatorLogo';
--- src/shared/ui/Layout/Layout.tsx
+++ src/shared/ui/Layout/Layout.tsx
@@ -1,10 +1,25 @@
-import { Outlet } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
+
+import { IncubatorLogo } from '@/shared/assets/icons/IncubatorLogo';
+import { Routes } from '@/shared/constants/routes';
+import { Button } from '@/shared/ui/Button';
+
+import styles from './Layout.module.scss';
 
 export const Layout = () => {
   return (
     <>
-      <h1>Header</h1>
-      <Outlet />
+      <header className={styles.header}>
+        <a href="https://it-incubator.io" rel="noreferrer" target="_blank">
+          <IncubatorLogo />
+        </a>
+        <Button as={Link} to={Routes.LOGIN} variant="secondary">
+          Sign in
+        </Button>
+      </header>
+      <main className={styles.main}>
+        <Outlet />
+      </main>
     </>
   );
 };

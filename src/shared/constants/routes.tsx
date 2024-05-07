4e0401d5340f1dddb0ab12a1d83d3442ed30735c
mabar84 <mabar@tut.by>
2024-05-07 20:47:36 +0300

    forgot-password changed to recover-password

--- src/shared/constants/routes.tsx
+++ src/shared/constants/routes.tsx
@@ -2,7 +2,7 @@ export const Routes = {
   CHECK_EMAIL: '/check-email',
   CREATE_NEW_PASSWORD: '/create-new-password',
   EDIT_PROFILE: '/edit-profile',
-  FORGOT_PASSWORD: '/forgot-password',
+  FORGOT_PASSWORD: '/recover-password',
   LOGIN: '/login',
   MAIN: '/',
   SIGN_UP: '/sign-up',

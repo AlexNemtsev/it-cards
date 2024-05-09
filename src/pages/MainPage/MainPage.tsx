7e82590b61fb5ad80d336851a0a4a4e4d7873843
mabar84 <mabar@tut.by>
2024-05-09 11:22:59 +0300

    MainPage refactored

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,25 +1,8 @@
-import { useLogoutMutation } from '@/entities/auth/api/auth';
-import { useGetDecksQuery } from '@/entities/deck/api/api';
-
 export const MainPage = () => {
-  const result = useGetDecksQuery();
-
-  console.log(result);
-
-  const [logout] = useLogoutMutation();
-
-  const onClickHandler = () => {
-    logout({});
-  };
-
   return (
     <div>
       MainPage
       <p>Аутентификация прошла успешно</p>
-      <p>Теперь можно вылогиниться</p>
-      <button onClick={onClickHandler} style={{ color: 'red' }}>
-        Logout
-      </button>
     </div>
   );
 };

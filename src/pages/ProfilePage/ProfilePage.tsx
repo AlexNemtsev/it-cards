5a23bc7198775ede496c4a4608c08082b24b4079
mabar84 <mabar@tut.by>
2024-05-29 00:48:29 +0300

    wrapper added, props name added

--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -1,19 +1,24 @@
 import { PersonalInformation } from '@/components/profile/PersonalInformation';
+import { useMeQuery } from '@/entities/auth/api/auth';
 import { useLogout } from '@/entities/auth/api/hooks';
+import { PageContainer } from '@/shared/ui/PageContainer';
 
 export const ProfilePage = () => {
   const [logout] = useLogout();
+  const { data } = useMeQuery();
 
   const logoutHandler = async () => {
     await logout();
   };
 
   return (
-    <PersonalInformation
-      logout={logoutHandler}
-      name="asd"
-      onSubmit={() => {}}
-      setAvatar={() => {}}
-    />
+    <PageContainer>
+      <PersonalInformation
+        logout={logoutHandler}
+        name={data?.name ?? ''}
+        onSubmit={() => {}}
+        setAvatar={() => {}}
+      />
+    </PageContainer>
   );
 };

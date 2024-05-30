f295cfc99c6ee632bcb7bf81d14c8beaf704538c
EvgenyStrunevsky <esstrynevskiy@gmail.com>
2024-05-30 12:15:49 +0300

    feature/84-add-DecksPage-types.ts-from-deck/api

--- src/entities/deck/api/api.ts
+++ src/entities/deck/api/api.ts
@@ -1,5 +1,7 @@
 import { flashcardsApi } from '@/shared/api/flashcardsApi';
 
+import { GetDecksArgs, getDecksResponse, getMinMaxCardsResponse } from './types';
+
 export const deckApi = flashcardsApi.injectEndpoints({
   endpoints: builder => {
     return {
@@ -21,47 +23,3 @@ export const deckApi = flashcardsApi.injectEndpoints({
 });
 
 export const { useGetDecksQuery, useGetMinMaxCardsQuery } = deckApi;
-
-export type Author = {
-  id: string;
-  name: string;
-};
-
-export type Deck = {
-  author: Author;
-  cardsCount: number;
-  cover: string;
-  created: string;
-  id: string;
-  isPrivate: boolean;
-  name: string;
-  updated: string;
-  userId: string;
-};
-
-export type Pagination = {
-  currentPage: number;
-  itemsPerPage: number;
-  totalItems: number;
-  totalPages: number;
-};
-
-export type getDecksResponse = {
-  items: Deck[];
-  pagination: Pagination;
-};
-
-export type GetDecksArgs = {
-  authorId?: string;
-  currentPage?: number;
-  itemsPerPage?: number;
-  maxCardsCount?: number;
-  minCardsCount?: number;
-  name?: string;
-  orderBy?: string;
-};
-
-export type getMinMaxCardsResponse = {
-  max: number;
-  min: number;
-};

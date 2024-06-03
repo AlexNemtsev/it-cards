4e880ff691055e7e20f90574e630bc5cdb7092fa
EvgenyStrunevsky <esstrynevskiy@gmail.com>
2024-06-03 12:42:36 +0300

    feature/84-add-DecksPage-DecksModal.tsx

---
+++ src/widgets/decks/DecksModal/DecksModal.tsx
@@ -0,0 +1,13 @@
+import { Modal } from '@/shared/ui/Modal';
+
+import { NewDeckForm } from './NewDeckForm';
+import { NewDeckTitle } from './ui/NewDeckTitle';
+import { OpenNewDeckModalButton } from './ui/OpenNewDeckModalButton';
+
+export const DecksModal = () => {
+  return (
+    <Modal title={<NewDeckTitle />} trigger={<OpenNewDeckModalButton />}>
+      <NewDeckForm onSubmit={data => alert(data)} />
+    </Modal>
+  );
+};

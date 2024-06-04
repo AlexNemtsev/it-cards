52dcd720645e4a9f2eaf1743776965585260ae73
mabar84 <mabar@tut.by>
2024-06-04 14:39:33 +0300

    for PR

--- src/pages/Page404/Page404.tsx
+++ src/pages/Page404/Page404.tsx
@@ -3,19 +3,19 @@ import { Link } from 'react-router-dom';
 import img404 from '@/shared/assets/img/404.png';
 import { Routes } from '@/shared/constants/routes';
 import { Button } from '@/shared/ui/Button';
-import { Page } from '@/shared/ui/Page/Page';
+import { PageContainer } from '@/shared/ui/PageContainer';
 import { Typography } from '@/shared/ui/Typography';
 
 import s from './Page404.module.scss';
 
 export const Page404 = () => {
   return (
-    <Page className={s.page404} mt={126}>
+    <PageContainer className={s.page404} mt={126}>
       <img alt="404" className={s.img} src={img404} />
       <Typography.Body1 className={s.text}>Sorry! Page not found!</Typography.Body1>
       <Button as={Link} to={Routes.MAIN}>
         Back to home page
       </Button>
-    </Page>
+    </PageContainer>
   );
 };

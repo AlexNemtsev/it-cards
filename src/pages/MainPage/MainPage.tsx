c00f22dda4a694ed3ea98c7816a1539043d94d08
mabar84 <mabar@tut.by>
2024-06-07 14:14:54 +0300

    for PR

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -6,9 +6,6 @@ export const MainPage = () => {
   return (
     <>
       <div>MainPage</div>
-      {/*<Link to={`${Routes.DECKS}/clr3b62x2051uzk2vxpvfbrbm`}>Колода</Link>*/}
-
-      {/*for qw@qw.qw*/}
       <Link to={`${Routes.DECKS}/clx34f5fp03puo501nv25lbzc`}>Колода</Link>
     </>
   );

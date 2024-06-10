473572d5eb36fc5cbd19604d1d4a80a7952c86b6
EvgenyStrunevsky <esstrynevskiy@gmail.com>
2024-06-10 11:57:46 +0300

    feature/84-add-DecksPage-return-restore-Page404-in-router

--- src/app/router/router.tsx
+++ src/app/router/router.tsx
@@ -53,11 +53,6 @@ const privateRoutes: RouteObject[] = [
     ],
     element: <Layout />,
   },
-  {
-    element: <Page404 />,
-    errorElement: <Page404 />,
-    path: '*',
-  },
 ];
 
 export const router = createBrowserRouter([

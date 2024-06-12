18b9f958b943d0204dafdc3981c35d5104687f10
mabar84 <mabar@tut.by>
2024-06-12 16:11:34 +0300

    CardsTable with Pagination in the same file

--- src/pages/DeckPage/DeckPage.tsx
+++ src/pages/DeckPage/DeckPage.tsx
@@ -1,6 +1,5 @@
 import { useParams, useSearchParams } from 'react-router-dom';
 
-import { useGetCardsQuery } from '@/entities/card/api/cardApi';
 import { useGetDeckQuery } from '@/entities/deck/api/deckApi';
 import { useMeQuery } from '@/entities/user/api';
 import { AddNewCardModal } from '@/pages/DeckPage/ui/AddNewCardModal/AddNewCardModal';
@@ -10,7 +9,6 @@ import { Routes } from '@/shared/constants/routes';
 import { Button } from '@/shared/ui/Button';
 import { DebouncedInput } from '@/shared/ui/DebouncedInput';
 import { PageContainer } from '@/shared/ui/PageContainer';
-import { Pagination } from '@/shared/ui/Pagination';
 import { Typography } from '@/shared/ui/Typography';
 import { CardsTable } from '@/widgets/Deck/CardsTable';
 
@@ -27,38 +25,30 @@ export const DeckPage = () => {
   const itemsPerPage = Number(searchParams.get('itemsPerPage')) || 10;
   const orderBy = searchParams.get('orderBy') || 'updated-desc';
 
-  const { data: cards } = useGetCardsQuery({
-    currentPage,
-    deckId,
-    itemsPerPage,
-    orderBy,
-    question,
-  });
-  const pagination = cards?.pagination ?? {
-    currentPage: 1,
-    itemsPerPage: 0,
-    totalItems: 0,
-    totalPages: 1,
-  };
   const utilSetSearchParams = (key: string, value: string) => {
     searchParams.set(key, value);
     setSearchParams(searchParams);
   };
+
   const changeSearchValue = (value: string) => {
     utilSetSearchParams('question', value);
     utilSetSearchParams('currentPage', '1');
   };
+
   const onInputClear = () => {
     utilSetSearchParams('question', '');
     utilSetSearchParams('currentPage', '1');
   };
+
   const onPaginationChange = (value: number) => {
     utilSetSearchParams('currentPage', value.toString());
   };
+
   const onItemsPerPageChange = (value: string) => {
     utilSetSearchParams('itemsPerPage', value);
     utilSetSearchParams('currentPage', '1');
   };
+
   const onOrderByChange = (orderBy: string) => {
     utilSetSearchParams('orderBy', orderBy);
   };
@@ -85,17 +75,16 @@ export const DeckPage = () => {
         placeholder="Search by question"
         type="search"
       />
-      {cards && (
-        <CardsTable data={cards.items} orderByCallBack={onOrderByChange} orderByKey={orderBy} />
-      )}
-      <Pagination
-        className={s.pagination}
-        currentPage={pagination.currentPage}
-        itemsPerPage={pagination.itemsPerPage.toString()}
-        itemsPerPageList={['10', '20', '30', '50', '100']}
+
+      <CardsTable
+        currentPage={currentPage}
+        itemsPerPage={itemsPerPage}
         onItemsPerPageChange={onItemsPerPageChange}
-        onValueChange={onPaginationChange}
-        totalPages={pagination.totalPages}
+        onPaginationChange={onPaginationChange}
+        orderBy={orderBy}
+        orderByCallBack={onOrderByChange}
+        orderByKey={orderBy}
+        question={question}
       />
     </PageContainer>
   );

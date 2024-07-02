d2812074760059c730b0e26f4fdd99741af33309
mabar84 <mabar@tut.by>
2024-07-02 08:17:06 +0300

    learn added

--- src/pages/DeckPage/ui/MyDeckDropdownMenu/MyDeckDropdownMenu.tsx
+++ src/pages/DeckPage/ui/MyDeckDropdownMenu/MyDeckDropdownMenu.tsx
@@ -1,7 +1,10 @@
+import { Link, useParams } from 'react-router-dom';
+
 import { Burger } from '@/shared/assets/icons/Burger/Burger';
 import { Delete } from '@/shared/assets/icons/Delete/Delete';
 import { Edit } from '@/shared/assets/icons/Edit/Edit';
 import { Learn } from '@/shared/assets/icons/Learn/Learn';
+import { Routes } from '@/shared/constants/routes';
 import { Dropdown } from '@/shared/ui/Dropdown';
 import { DropdownItem } from '@/shared/ui/Dropdown/DropdownItem';
 import { DropdownItemDivider } from '@/shared/ui/Dropdown/DropdownItemDivider';
@@ -10,13 +13,15 @@ import { Typography } from '@/shared/ui/Typography';
 import s from './MyDeckDropdownMenu.module.scss';
 
 export const MyDeckDropdownMenu = () => {
+  const { [Routes.DECK_ID]: deckId = '' } = useParams();
+
   return (
     <Dropdown className={s.dropdown} trigger={<Burger />}>
       <DropdownItem>
-        <button className={s.dropdownItemButton}>
+        <Link className={s.dropdownItemButton} to={`${Routes.DECKS}/${deckId}/learn`}>
           <Learn />
           <Typography.Caption>Learn</Typography.Caption>
-        </button>
+        </Link>
       </DropdownItem>
       <DropdownItemDivider />
       <DropdownItem>

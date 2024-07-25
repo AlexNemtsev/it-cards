fc3e48270e1f9e548a7bbdc31a5b7e286179cad0
mabar84 <mabar@tut.by>
2024-07-25 14:45:21 +0300

    IncubatorLogo moved

--- src/app/Layout/ui/IncubatorLogo/IncubatorLogo.tsx
+++ src/app/Layout/ui/IncubatorLogo/IncubatorLogo.tsx
@@ -1,6 +1,6 @@
+import { IncubatorLogoBig } from '@/app/Layout/ui/IncubatorLogo/IncubatorLogoBig';
+import { IncubatorLogoSmall } from '@/app/Layout/ui/IncubatorLogo/IncubatorLogoSmall';
 import { breakpoints } from '@/app/styles/breakpoints';
-import { IncubatorLogoBig } from '@/shared/assets/icons/IncubatorLogo/IncubatorLogoBig';
-import { IncubatorLogoSmall } from '@/shared/assets/icons/IncubatorLogo/IncubatorLogoSmall';
 import { useMediaQuery } from '@/shared/hooks/useMediaQuery';
 
 export const IncubatorLogo = () => {

ef7f429aa62c07deda51340e2c65c9a0a32fdbe9
EvgenyStrunevsky <esstrynevskiy@gmail.com>
2024-08-07 11:12:21 +0300

    feature/155-responsiveDecksPage:-mobileDecksTable

--- src/widgets/decks/DecksTable/DecksTable.tsx
+++ src/widgets/decks/DecksTable/DecksTable.tsx
@@ -1,11 +1,13 @@
 import { Link } from 'react-router-dom';
 
+import { breakpoints } from '@/app/styles/breakpoints';
 import { useGetDecksQuery, useGetMinMaxCardsQuery } from '@/entities/deck/api/deckApi';
 import { useMeQuery } from '@/entities/user/api';
 import { useDecksSearchParams } from '@/pages/DecksPage/useDecksSearchParams';
 import { ChevronDownIcon } from '@/shared/assets/icons/ChevronDownIcon';
 import { ChevronUpIcon } from '@/shared/assets/icons/ChevronUpIcon';
 import { Routes } from '@/shared/constants/routes';
+import { useMediaQuery } from '@/shared/hooks/useMediaQuery';
 import { formatDate } from '@/shared/lib/formatDate';
 import { isDateValid } from '@/shared/lib/isDateValid';
 import { Pagination } from '@/shared/ui/Pagination';
@@ -87,6 +89,8 @@ export const DecksTable = () => {
     getOrderDecksBy(orderVariants.created);
   };
 
+  const isTablet = useMediaQuery(`(max-width: ${breakpoints.tablet})`);
+
   return (
     <>
       {isError && <div>Error...</div>}
@@ -95,66 +99,118 @@ export const DecksTable = () => {
       ) : (
         decks &&
         decks.items.length > 0 && (
-          <div className="decks">
-            <Table className={s.decks}>
-              <TableHead>
-                <TableRow>
-                  <TableHeadCell className={s.head} onClick={sortByName}>
-                    <span className={s.headName}>Name</span> {setOrderIcon(orderVariants.name)}
-                  </TableHeadCell>
-                  <TableHeadCell className={s.head} onClick={sortByCardsCount}>
-                    <span className={s.headName}>Cards</span>{' '}
-                    {setOrderIcon(orderVariants.cardsCount)}
-                  </TableHeadCell>
-                  <TableHeadCell className={s.head} onClick={sortByUpdated}>
-                    <span className={s.headName}>Last Updated</span>{' '}
-                    {setOrderIcon(orderVariants.updated)}
-                  </TableHeadCell>
-                  <TableHeadCell className={s.head} onClick={sortByCreated}>
-                    <span className={s.headName}>Created by</span>{' '}
-                    {setOrderIcon(orderVariants.created)}
-                  </TableHeadCell>
-                  <TableHeadCell className={s.head}></TableHeadCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
+          <>
+            {isTablet ? (
+              <div className={s.mobileDecks}>
                 {decks.items.map(item => (
-                  <TableRow key={item.id}>
-                    <TableCell>
-                      <Link className={s.linkToDeck} to={`${Routes.DECKS}/${item.id}`}>
-                        {item.cover && <img alt="cover" className={s.cover} src={item.cover} />}
-                        <Typography.Body2 className={s.name}>{item.name}</Typography.Body2>
-                      </Link>
-                    </TableCell>
-                    <TableCell>{item.cardsCount}</TableCell>
-                    <TableCell>{isDateValid(item.updated) && formatDate(item.updated)}</TableCell>
-                    <TableCell>{isDateValid(item.created) && formatDate(item.created)}</TableCell>
-                    <TableCell>
-                      {authorId && (
-                        <>
+                  <div className={s.mobileDeck} key={item.id}>
+                    <Link className={s.mobileLinkToDeck} to={`${Routes.DECKS}/${item.id}`}>
+                      <div className={s.row}>
+                        <div className={s.name}>name</div>
+                        <div className={s.packName}>{item.name}</div>
+                      </div>
+                      <div className={s.row}>
+                        <div className={s.cards}>Cards</div>
+                        <div className={s.cardsCount}>{item.cardsCount}</div>
+                      </div>
+                      <div className={s.row}>
+                        <div className={s.lastUpdated}>Last Updated</div>
+                        <div className={s.lastUpdatedDate}>
+                          {isDateValid(item.updated) && formatDate(item.updated)}
+                        </div>
+                      </div>
+                      <div className={s.row}>
+                        <div className={s.createdBy}>Created</div>
+                        <div className={s.createdByName}>
+                          {isDateValid(item.created) && formatDate(item.created)}
+                        </div>
+                      </div>
+                    </Link>
+                    {authorId && (
+                      <div className={s.mobileDecksTools}>
+                        <div className={s.decksTool}>
                           <DeleteDeckModal id={item.id} />
+                        </div>
+                        <div className={s.decksTool}>
                           <EditDeckModal
                             cover={item.cover}
                             id={item.id}
                             isPrivate={item.isPrivate}
                             name={item.name}
                           />
-                        </>
-                      )}
-                    </TableCell>
-                  </TableRow>
+                        </div>
+                      </div>
+                    )}
+                  </div>
                 ))}
-              </TableBody>
-            </Table>
-            <Pagination
-              currentPage={currentPage || 1}
-              itemsPerPage={String(itemsPerPage) || '10'}
-              itemsPerPageList={ITEMS_PER_PAGE_VARIANTS}
-              onItemsPerPageChange={getItemsPerPage}
-              onValueChange={getCurrentPage}
-              totalPages={decks.pagination.totalPages || 1}
-            />
-          </div>
+              </div>
+            ) : (
+              <div className="decks">
+                <Table className={s.decks}>
+                  <TableHead>
+                    <TableRow>
+                      <TableHeadCell className={s.head} onClick={sortByName}>
+                        <span className={s.headName}>Name</span> {setOrderIcon(orderVariants.name)}
+                      </TableHeadCell>
+                      <TableHeadCell className={s.head} onClick={sortByCardsCount}>
+                        <span className={s.headName}>Cards</span>{' '}
+                        {setOrderIcon(orderVariants.cardsCount)}
+                      </TableHeadCell>
+                      <TableHeadCell className={s.head} onClick={sortByUpdated}>
+                        <span className={s.headName}>Last Updated</span>{' '}
+                        {setOrderIcon(orderVariants.updated)}
+                      </TableHeadCell>
+                      <TableHeadCell className={s.head} onClick={sortByCreated}>
+                        <span className={s.headName}>Created by</span>{' '}
+                        {setOrderIcon(orderVariants.created)}
+                      </TableHeadCell>
+                      <TableHeadCell className={s.head}></TableHeadCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {decks.items.map(item => (
+                      <TableRow key={item.id}>
+                        <TableCell>
+                          <Link className={s.linkToDeck} to={`${Routes.DECKS}/${item.id}`}>
+                            {item.cover && <img alt="cover" className={s.cover} src={item.cover} />}
+                            <Typography.Body2 className={s.name}>{item.name}</Typography.Body2>
+                          </Link>
+                        </TableCell>
+                        <TableCell>{item.cardsCount}</TableCell>
+                        <TableCell>
+                          {isDateValid(item.updated) && formatDate(item.updated)}
+                        </TableCell>
+                        <TableCell>
+                          {isDateValid(item.created) && formatDate(item.created)}
+                        </TableCell>
+                        <TableCell>
+                          {authorId && (
+                            <>
+                              <DeleteDeckModal id={item.id} />
+                              <EditDeckModal
+                                cover={item.cover}
+                                id={item.id}
+                                isPrivate={item.isPrivate}
+                                name={item.name}
+                              />
+                            </>
+                          )}
+                        </TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+                <Pagination
+                  currentPage={currentPage || 1}
+                  itemsPerPage={String(itemsPerPage) || '10'}
+                  itemsPerPageList={ITEMS_PER_PAGE_VARIANTS}
+                  onItemsPerPageChange={getItemsPerPage}
+                  onValueChange={getCurrentPage}
+                  totalPages={decks.pagination.totalPages || 1}
+                />
+              </div>
+            )}
+          </>
         )
       )}
       {!isLoading && decks?.items.length === 0 && <div>Empty</div>}
